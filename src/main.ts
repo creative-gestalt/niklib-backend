5af6044f7766d8959124f5a5b4c747fb37bd3362
thegestalt <lordweasel6@gmail.com>
2022-09-27 17:31:47 -0600

    ooooop

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as http from 'http';
+import express from 'express';
+import * as https from 'https';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +21,10 @@ async function bootstrap() {
     },
   };
   app.enableCors(corsOptions);
-  await app.listen(process.env.PORT || 443);
+  // await app.listen(process.env.PORT || 3000);
+  const server = express();
+  await app.init();
+  http.createServer(server).listen(3000);
+  https.createServer(server).listen(443);
 }
 bootstrap();
